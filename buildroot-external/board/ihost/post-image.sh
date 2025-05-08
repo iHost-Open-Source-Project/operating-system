f778aeb04f769413c065637b14d15627fcf8d688
hao.zhang <hao.zhang@coolkit.cn>
2025-05-08 21:34:26 +0800

    use ha 6.12

--- buildroot-external/board/ihost/post-image.sh
+++ buildroot-external/board/ihost/post-image.sh
@@ -15,7 +15,7 @@ fi
 
 # compile kernel zboot.img
 cp ${BUILD_DIR}/rockchip-rkbin-b4558da0860ca48bf1a571dd33ccba580b9abe23/tools/mkimage ${BINARIES_DIR}/
-fitimage "${BINARIES_DIR}/zboot.img" "${BOARD_DIR}/boot.its" "${BINARIES_DIR}/zImage" "${BINARIES_DIR}/rv1126-ihost.dtb"
+fitimage "${BINARIES_DIR}/zboot.img" "${BOARD_DIR}/boot.its" "${BINARIES_DIR}/zImage" "${BINARIES_DIR}/rv1109-sonoff-ihost.dtb"
 
 # # generate image
 gen_image
