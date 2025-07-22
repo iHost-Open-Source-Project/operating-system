63e0518cf8db6f55612d6bc4c4fea1fc155259cf
peiwei.yao <peiwei.yao@coolkit.cn>
2025-07-22 18:12:13 +0800

    feat: add emmc setting wpaconf feature

--- buildroot-external/genimage/genimage.sh
+++ buildroot-external/genimage/genimage.sh
@@ -3,6 +3,14 @@
 GENIMAGE_DIR="${BR2_EXTERNAL_LINUX_DISTRO_PATH}/genimage"
 
 function gen_image() {
+    # delete the image if it exists
+    if [ -f "${BINARIES_DIR}/${IMAGE_NAME}.img" ]; then
+        rm -f "${BINARIES_DIR}/${IMAGE_NAME}.img"
+    fi
+    if [ -f "${BINARIES_DIR}/${IMAGE_NAME}.img.xz" ]; then
+        rm -f "${BINARIES_DIR}/${IMAGE_NAME}.img.xz"
+    fi
+
     trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
     ROOTPATH_TMP="$(mktemp -d)"
 
@@ -18,4 +26,7 @@ function gen_image() {
         --inputpath "${BINARIES_DIR}"  \
         --outputpath "${BINARIES_DIR}" \
         --config "${GENIMAGE_DIR}/genimage.cfg"
+
+    # compress the image
+    xz -T0 ${BINARIES_DIR}/${IMAGE_NAME}.img
 }
